42de0f740744a4c105707640aaee328df1b8837f
Isaac Akalanne <iakalann@jaguarlandrover.com>
2023-04-23 13:13:19 +0100

    Added additional UI test

--- Cabify Shop/Cabify ShopUITests/Cabify_ShopUITests.swift
+++ Cabify Shop/Cabify ShopUITests/Cabify_ShopUITests.swift
@@ -22,6 +22,37 @@ final class Cabify_ShopUITests: XCTestCase {
         XCTAssertFalse(proceedToCheckoutButton.waitForExistence(timeout: TimeInterval(timeout)))
     }
     
+    func testAddThenDelete3MugsFromCart() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let timeout = 2
+        let productString = "MUG"
+        
+        let addQuantityButton = app.buttons["horizontal\(productString)AddQuantityButton"]
+        XCTAssertTrue(addQuantityButton.waitForExistence(timeout: TimeInterval(timeout)))
+        addQuantityButton.tap()
+        addQuantityButton.tap()
+        addQuantityButton.tap()
+        
+        let addToCartButton = app.buttons["cart\(productString)CircleButton"]
+        XCTAssertTrue(addToCartButton.waitForExistence(timeout: TimeInterval(timeout)))
+        addToCartButton.tap()
+        
+        let viewYourCartButton = app.buttons["viewYourCartButton"]
+        XCTAssertTrue(viewYourCartButton.waitForExistence(timeout: TimeInterval(timeout)))
+        viewYourCartButton.tap()
+        
+        let priceText = app.staticTexts["\(productString)NoDiscountPriceText"]
+        XCTAssertTrue(priceText.waitForExistence(timeout: TimeInterval(timeout)))
+        
+        let trashButton = app.buttons["trash\(productString)CircleButton"]
+        XCTAssertTrue(trashButton.waitForExistence(timeout: TimeInterval(timeout)))
+        trashButton.tap()
+        
+        XCTAssertFalse(priceText.waitForExistence(timeout: TimeInterval(timeout)))
+    }
+    
     func testBuy1Mug() throws {
         let app = XCUIApplication()
         app.launch()
